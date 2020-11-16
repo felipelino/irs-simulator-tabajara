2820139d1f235f6049320b85807e1d8ff2eeeebf
Felipe Lino <felipe.lino@farfetch.com>
2020-11-16 10:30:32 +0000

    feat: fix report when Married2

--- src/main/java/lino/irs/pt/output/Report.java
+++ src/main/java/lino/irs/pt/output/Report.java
@@ -1,6 +1,7 @@
 package lino.irs.pt.output;
 
 import lino.irs.pt.input.IRSRequest;
+import lino.irs.pt.input.SituationTypeEnum;
 import lino.irs.pt.service.Calculator;
 
 import java.math.BigDecimal;
@@ -20,11 +21,17 @@ public class Report {
         if(request.getTotalTSU().compareTo(BigDecimal.ZERO) != 0) {
             reportAsString = "\nTotal TSU retido na fonte...:  "+ request.getTotalTSU().setScale(2, BigDecimal.ROUND_HALF_UP);
         }
+
+        BigDecimal deduction = Calculator.FIXED_DEDUCTION;
+        if(request.getSituationTypeEnum() == SituationTypeEnum.Married2) {
+            deduction = Calculator.FIXED_DEDUCTION.multiply(new BigDecimal(2));
+        }
+
         reportAsString +=
                 "\nValor bruto total recebido..:  "+ request.getTotalGross().setScale(2, BigDecimal.ROUND_HALF_UP) +
                 "\n\t = Total bruto salario....:  "+ request.getTotalMonthlyIncome().setScale(2, BigDecimal.ROUND_HALF_UP) +
                 "\n\t + Total RSUs Vested......:  "+ request.getTotalRSUsInEuro().setScale(2, BigDecimal.ROUND_HALF_UP) +
-                "\nDeducao fixa................: -"+ Calculator.FIXED_DEDUCTION.setScale(2, BigDecimal.ROUND_HALF_UP) +
+                "\nDeducao fixa................: -"+ deduction.setScale(2, BigDecimal.ROUND_HALF_UP) +
                 "\n\nTotal sujeito a tributo.....:  "+summary.getAmountSubjectToTax() +
                 "\nValor total de IRS..........:  "+summary.getTotalIrs() +
                 "\nValor de IRS retido na fonte: -"+ request.getTotalRetention().setScale(2, BigDecimal.ROUND_HALF_UP);
