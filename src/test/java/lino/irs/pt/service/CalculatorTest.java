49553a0e5664f3f3ca7ccdf5c6b56c652c51d3d8
Felipe Lino <felipe.lino@farfetch.com>
2022-01-21 13:35:38 +0000

    fix: unit test

--- src/test/java/lino/irs/pt/service/CalculatorTest.java
+++ src/test/java/lino/irs/pt/service/CalculatorTest.java
@@ -13,10 +13,10 @@ import java.math.BigDecimal;
 public class CalculatorTest {
 
     @Test
-    public void calculateIRSTestWithoutVests() {
+    public void calculateIRSTest_Married2_WithoutVests_WithISSLessThanRetention() {
 
         MonthlyIncomeList monthlyIncomes = new MonthlyIncomeList();
-        monthlyIncomes.add(new BigDecimal(35000), new BigDecimal(8540), new BigDecimal(3850));
+        monthlyIncomes.add(new BigDecimal(35000), new BigDecimal(8540), new BigDecimal(1850));
         monthlyIncomes.add(new BigDecimal(18200), new BigDecimal(2380), new BigDecimal(2002));
         IRSRequest request = new IRSRequest(SituationTypeEnum.Married2, monthlyIncomes, new StockShareList());
         IRSSummary summary = Calculator.calculateIRS(request);
@@ -24,5 +24,32 @@ public class CalculatorTest {
         Assert.assertEquals("44992.00", summary.getAmountSubjectToTax() + "");
         Assert.assertEquals("10715.78", summary.getTotalIrs() + "");
         Assert.assertEquals("-204.22", summary.getIrsToPay() + "");
+    }
+
+    @Test
+    public void calculateIRSTest_Married2_WithoutVests_WithISSLessThanRetention_SingleLine() {
+
+        MonthlyIncomeList monthlyIncomes = new MonthlyIncomeList();
+        monthlyIncomes.add(new BigDecimal(53200), new BigDecimal(10920), new BigDecimal(3852));
+        IRSRequest request = new IRSRequest(SituationTypeEnum.Married2, monthlyIncomes, new StockShareList());
+        IRSSummary summary = Calculator.calculateIRS(request);
+        Assert.assertNotNull(summary);
+        Assert.assertEquals("44992.00", summary.getAmountSubjectToTax() + "");
+        Assert.assertEquals("10715.78", summary.getTotalIrs() + "");
+        Assert.assertEquals("-204.22", summary.getIrsToPay() + "");
+    }
+
+    @Test
+    public void calculateIRSTest_Married2_WithoutVests_WithISSMoreThanRetention() {
+
+        MonthlyIncomeList monthlyIncomes = new MonthlyIncomeList();
+        BigDecimal tsuRetention = Calculator.FIXED_DEDUCTION.multiply(new BigDecimal(3));
+        monthlyIncomes.add(new BigDecimal(53200), new BigDecimal(10920), tsuRetention);
+        IRSRequest request = new IRSRequest(SituationTypeEnum.Married2, monthlyIncomes, new StockShareList());
+        IRSSummary summary = Calculator.calculateIRS(request);
+        Assert.assertNotNull(summary);
+        Assert.assertEquals("40888.00", summary.getAmountSubjectToTax() + "");
+        Assert.assertEquals("9279.38", summary.getTotalIrs() + "");
+        Assert.assertEquals("-1640.62", summary.getIrsToPay() + "");
     }
 }
